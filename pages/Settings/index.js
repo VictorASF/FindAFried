8e5f04b2fe61abecca8a13cc858fee3df1b1d218
Victor Fonseca <vasf111@hotmail.com>
2021-05-30 03:25:16 -0300

    Change input

--- pages/Settings/index.js
+++ pages/Settings/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext, useState } from 'react';
-import { } from 'react-native';
+import { Text } from 'react-native';
 
 import {
   Container,
@@ -25,22 +25,38 @@ import { useFocusEffect } from '@react-navigation/native';
 export default function Settings({route, navigation}) {
 
   const [ usuario, setUsuario ] = useState([{name:'',telefone:'',endereco:''}])
+  const [ UsuarioName, setUsuarioName ] = useState('')
+  const [ UsuarioTelefone, setUsuarioTelefone] = useState('')
+  const [ UsuarioEndereco, setUsuarioEndereco] = useState('')
+  const [ UsuarioUid, setUsuarioUid ] = useState('')
+  const [ UsuarioId, setUsuarioId ] = useState('')
   const { signOut } = useContext(UsuarioContext)
   const { user } = useContext(UsuarioContext)
 
   const ListenUpdate = (snap) => {
     const data = snap.docs.map((doc)=>{
+      setUsuarioId(doc.id)
+      console.warn(UsuarioId)
       return{
         uid:doc.id,
         ... doc.data()
       }
       
     })
-    setUsuario(data)
+    setUsuarioName(data[0].name)
+    setUsuarioTelefone(data[0].telefone)
+    setUsuarioEndereco(data[0].endereco)
+    setUsuarioUid(data[0].uid)
   }
 
+  // const UpdateDb = () =>{
+  // firebase.firestore().collection('Users').doc(`${UsuarioId}`)
+  // .update({name: UsuarioName,
+  // telefone: UsuarioTelefone,
+  // endereco: UsuarioEndereco})
+  // }
+  
   useFocusEffect(React.useCallback(()=>{
-    console.warn('To aqui')
     firebase.firestore().collection('Users').where('uid','==', user.uid).onSnapshot(ListenUpdate);
   },[]))
 
@@ -56,25 +72,25 @@ export default function Settings({route, navigation}) {
       <ContainerFoto>
         <Foto/>
       </ContainerFoto>
+      
       <ContainerInfo>
         <InfoText>
           Nome:
-          {usuario[0].name}
         </InfoText>
         <ContainerBox>
-        <Box/>
+        <Box onChangeText={setUsuarioName} value = {UsuarioName}/>
         </ContainerBox>
         <InfoText>
           Telefone
         </InfoText>
         <ContainerBox>
-        <Box/>
+        <Box onChangeText={setUsuarioTelefone} value={UsuarioTelefone}/>
         </ContainerBox>
         <InfoText>
           Endereço
         </InfoText>
         <ContainerBox>
-        <Box/>
+        <Box onChangeText={setUsuarioEndereco} value={UsuarioEndereco}/>
         </ContainerBox>
       </ContainerInfo>
       <ContainerButtons>
